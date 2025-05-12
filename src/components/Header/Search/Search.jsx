4ed8479bb9632768ab13bff730b6f7d997e2562c
poldevy <rostyslavshyian@gmail.com>
2025-05-12 15:23:23 +0200

    styles for search

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -1,19 +1,63 @@
+import { useState, useEffect, useRef } from "react";
 import styles from "./Search.module.scss";
+import EarthIcon from "../../../assets/EarthIcon.svg?react";
+import SearchIcon from "../../../assets/SearchIcon.svg?react";
 
 import { useTranslation } from "react-i18next";
 
 export const Search = () => {
+  const [search, setSearch] = useState(true);
   const { i18n } = useTranslation();
+
+  const inputRef = useRef(true);
+  const containerRef = useRef(true);
 
   const toggleLanguage = () => {
     const newLang = i18n.language === "en" ? "pl" : "en";
     i18n.changeLanguage(newLang);
   };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(event.target)
+      ) {
+        setSearch(true);
+      }
+    };
+
+    if (!search) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [search]);
+
   return (
-    <div className={styles.search}>
-      <button onClick={toggleLanguage}>
+    <div className={styles.search} ref={containerRef}>
+      <button className={styles.btn} onClick={toggleLanguage}>
+        <EarthIcon />
         {i18n.language === "en" ? "Pl" : "En"}
       </button>
+
+      <div className={styles.inputWrapper}>
+        <input
+          type="text"
+          ref={inputRef}
+          placeholder="Search"
+          autoFocus={!search}
+          className={`${styles.input} ${!search ? styles.inputVisible : ""}`}
+        />
+      </div>
+
+      {search && (
+        <button onClick={() => setSearch(false)}>
+          <SearchIcon />
+        </button>
+      )}
     </div>
   );
 };
