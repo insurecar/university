b7b6bafa202762f8fe3240291a70a30b28860e1a
simon07gd <sgrzywna@griddynamics.com>
2025-06-13 17:52:49 +0200

    Add mock data and translations

---
+++ src/components/pages/Team/teamData.js
@@ -0,0 +1,329 @@
+export const honoraryCommittee = [
+  {
+    id: 's1',
+    name: 'Przemysław Wojtaszek',
+    title: 'Ekonomista, EFC',
+    image: 'assets/mockTeamMember.png',
+  },
+  {
+    id: 's2',
+    name: 'Bartosz Szczygieł',
+    title: 'Dyrektor Departamentu Inwestycji, BRG S.A.',
+    image: 'assets/mockTeamMember.png',
+  },
+  {
+    id: 's3',
+    name: 'Marek Rogalski',
+    title: 'Analityk walutowy, FxMarket.',
+    image: 'assets/mockTeamMember.png',
+  },
+  {
+    id: 's4',
+    name: 'Trader21',
+    title: 'Niezależny Inwestor',
+    image: 'assets/mockTeamMember.png',
+  },
+  {
+    id: 's5',
+    name: 'Jacek Jakóbczak',
+    title: 'CEO, ROZWOJOWCY.PL',
+    image: 'assets/mockTeamMember.png',
+  },
+  {
+    id: 's6',
+    name: 'Arkadiusz Regiec',
+    title: 'CEO, CODEBOX S.A.',
+    image: 'assets/mockTeamMember.png',
+  },
+  {
+    id: 's7',
+    name: 'Maciej Leśniczorz',
+    title: 'Ekspert CFD i Markets',
+    image: 'assets/mockTeamMember.png',
+  },
+  {
+    id: 's8',
+    name: 'Bartosz Graś',
+    title: 'Adwokat, Kancelaria Prawna',
+    image: 'assets/mockTeamMember.png',
+  },
+  {
+    id: 's9',
+    name: 'Michał Stanek',
+    title: 'Prezes Zarządu, QUALLE S.A.',
+    image: 'assets/mockTeamMember.png',
+  },
+  {
+    id: 's10',
+    name: 'Mateusz Wicielniak',
+    title: 'Prezes Zarządu, NOBLE MARKETS',
+    image: 'assets/mockTeamMember.png',
+  },
+  {
+    id: 's11',
+    name: 'Sylwester Sunszeck',
+    title: 'Prezes Zarządu, ENERGO S.A.',
+    image: 'assets/mockTeamMember.png',
+  },
+  {
+    id: 's12',
+    name: 'Piotr Karbowski',
+    title: 'Prezes Zarządu, CREATIVEFORMS GAMES S.A.',
+    image: 'assets/mockTeamMember.png',
+  },
+  {
+    id: 's13',
+    name: 'Albert "Longterm" Rokicki',
+    title: 'Inwestor długoterminowy',
+    image: 'assets/mockTeamMember.png',
+  },
+  {
+    id: 's14',
+    name: 'Marcin Werus',
+    title: 'Prezes Zarządu, INVEST COMMERCE',
+    image: 'assets/mockTeamMember.png',
+  },
+  {
+    id: 's15',
+    name: 'Mike Satoshi',
+    title: 'Entuzjasta kryptowalut',
+    image: 'assets/mockTeamMember.png',
+  },
+  {
+    id: 's16',
+    name: 'Cezary Andrzejczyk',
+    title: 'Ekspert ds. nieruchomości',
+    image: 'assets/mockTeamMember.png',
+  },
+];
+
+export const organisationCommittee = [
+    {
+      id: 'marketing-and-communication',
+      name: 'Marketing and Communication',
+      members: [
+        {
+          id: 'm1',
+          name: 'Anna Kowalska',
+          title: 'Communication Coordinator',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 'm2',
+          name: 'Jan Nowak',
+          title: 'Social Media Specialist',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 'm3',
+          name: 'Zofia Lis',
+          title: 'Marketing Director',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 'm4',
+          name: 'Piotr Kaczmarek',
+          title: 'PR Specialist',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 'm5',
+          name: 'Ewa Grabowska',
+          title: 'Graphic Designer',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 'm6',
+          name: 'Krzysztof Wójcik',
+          title: 'Content Manager',
+          image: 'assets/mockTeamMember.png',
+        },
+      ],
+    },
+    {
+      id: 'administrative-affairs',
+      name: 'Administrative Affairs',
+      members: [
+        {
+          id: 'a1',
+          name: 'Tomasz Rozmus',
+          title: 'Office Manager',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 'a2',
+          name: 'Mariola Konieczna',
+          title: 'Office Assistant',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 'a3',
+          name: 'Adam Wiśniewski',
+          title: 'Contract Specialist',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 'a4',
+          name: 'Natalia Dudek',
+          title: 'Documentation Coordinator',
+          image: 'assets/mockTeamMember.png',
+        },
+      ],
+    },
+    {
+      id: 'financial-affairs',
+      name: 'Financial Affairs',
+      members: [
+        {
+          id: 'f1',
+          name: 'Monika Zielińska',
+          title: 'Chief Accountant',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 'f2',
+          name: 'Kamil Wójcik',
+          title: 'Billing Specialist',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 'f3',
+          name: 'Dariusz Nowakowski',
+          title: 'Financial Analyst',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 'f4',
+          name: 'Alicja Czerwińska',
+          title: 'Cashier',
+          image: 'assets/mockTeamMember.png',
+        },
+      ],
+    },
+    {
+      id: 'technology-and-infrastructure',
+      name: 'Technology and Infrastructure',
+      members: [
+        {
+          id: 't1',
+          name: 'Piotr Miskurek',
+          title: 'IT Manager',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 't2',
+          name: 'Magdalena Grzyb',
+          title: 'Network Administrator',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 't3',
+          name: 'Grzegorz Piotrowski',
+          title: 'Frontend Developer',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 't4',
+          name: 'Joanna Jankowska',
+          title: 'Backend Developer',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 't5',
+          name: 'Michał Kowalczyk',
+          title: 'Technical Support Specialist',
+          image: 'assets/mockTeamMember.png',
+        },
+      ],
+    },
+    {
+      id: 'business-collaboration',
+      name: 'Business Collaboration',
+      members: [
+        {
+          id: 'b1',
+          name: 'Dewid Augustyn',
+          title: 'Partnership Director',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 'b2',
+          name: 'Bogusz Kasowski',
+          title: 'Key Account Specialist',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 'b3',
+          name: 'Maksymilian Bączkowski',
+          title: 'Contract Negotiator',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 'b4',
+          name: 'Krzysztof Mazurek',
+          title: 'Market Analyst',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 'b5',
+          name: 'Patrycja Sobieraj',
+          title: 'Business Advisor',
+          image: 'assets/mockTeamMember.png',
+        },
+      ],
+    },
+    {
+      id: 'community-outreach',
+      name: 'Community Outreach',
+      members: [
+        {
+          id: 'o1',
+          name: 'Agnieszka Król',
+          title: 'Public Relations Coordinator',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 'o2',
+          name: 'Marcin Chmielewski',
+          title: 'Community Engagement Specialist',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 'o3',
+          name: 'Karolina Dąbrowska',
+          title: 'Social Analyst',
+          image: 'assets/mockTeamMember.png',
+        },
+      ],
+    },
+    {
+      id: 'student-and-doctoral-collaboration',
+      name: 'Student and Doctoral Collaboration',
+      members: [
+        {
+          id: 's_d1',
+          name: 'Paulina Wróbel',
+          title: 'Student Club Supervisor',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 's_d2',
+          name: 'Michał Kowalski',
+          title: 'Student Programs Mentor',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 's_d3',
+          name: 'Julia Nowak',
+          title: 'Didactics Specialist',
+          image: 'assets/mockTeamMember.png',
+        },
+        {
+          id: 's_d4',
+          name: 'Robert Lewandowski',
+          title: 'Workshop Organizer',
+          image: 'assets/mockTeamMember.png',
+        },
+      ],
+    },
+  ];
