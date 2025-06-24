380cd90d24e9b6b8a3da443d2fc08b1bb5a22e3d
simon07gd <sgrzywna@griddynamics.com>
2025-06-24 16:44:01 +0200

    Add markup and styles for 404 page

--- src/components/pages/PageNotFound/PageNotFound.jsx
+++ src/components/pages/PageNotFound/PageNotFound.jsx
@@ -1,6 +1,12 @@
-export const PageNotFoundComponent = () => {
+import { Subtitle, Title } from "../../UI";
+import styles from "./PageNotFound.module.scss";
 
+export const PageNotFoundComponent = () => {
   return (
-    <div>404</div>
+    <div className={styles.pageNotFound}>
+      <h1 className={styles.error404}>404</h1>
+      <Title text="page-not-found.title" className={styles.title} />
+      <Subtitle text="page-not-found.subtitle" className={styles.subtitle} />
+    </div>
   );
 };
